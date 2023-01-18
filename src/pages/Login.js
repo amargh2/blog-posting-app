3410d93ec136f78cadb9a550cf68bf5d06399d79
amargh2 <margherioa@gmail.com>
2023-01-18 15:53:35 -0600

    installed axios and set up fetching

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+export default function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  return (
+    <div>
+      
+    </div>
+  )
+}
