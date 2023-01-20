5162bb2fb828fd789c94d35ee6a83d8ee5e80ec6
amargh2 <margherioa@gmail.com>
2023-01-19 18:48:12 -0600

    installed redux, setting up login route and component

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import NewPost from './pages/NewPost'
 import { Routes, Route } from "react-router-dom"
 import Nav from './pages/components/Nav'
 import Header from './pages/components/Header';
+
+//component class div is the container that holds the current view (post, view, login);
+//layout-setup class sets up 10column grid on medium+ screens and converts to a stacked column layout on mobile
 function App() {
   return (
-    //Header is present on every page;
     <div className="App">
       <div className='header pb-2'><Header/></div>
-      <div className="grid-setup grid grid-cols-10 mx-1">
-      <div className='nav sm:col-start-1 col-start-4'><Nav/></div>
-      <div className='component md:col-start-3 md:col-span-6 col-span-full mx-1'>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/new' element={<NewPost/>}></Route>
-          <Route path='/posts' element={<ViewPosts/>}></Route>
-        </Routes>
-      </div>
-      </div>
+      <div className="layout-setup flex flex-col md:grid md:grid-cols-10 mx-1">
+        <div className='nav flex justify-center sm:col-start-1 col-start-4'><Nav/></div>
+        <div className='component md:col-start-3 md:col-span-6 col-span-full mx-1'>
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/new' element={<NewPost/>}></Route>
+            <Route path='/posts' element={<ViewPosts/>}></Route>
+          </Routes>
+        </div>
+        </div>
     </div>
   );
 }
