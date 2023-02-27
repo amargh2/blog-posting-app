f71c35f8fafc9a96f27abee9f9624e9ac1d9167e
anthony <margherioa@gmail.com>
2023-02-27 16:32:21 -0600

    login page changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,22 +14,25 @@ export default function Login() {
   //dispatch defines the action that feeds into the reducer (ie, what are we doin here with this piece state?)
   const dispatch = useDispatch()
   console.log(token)
-  function login() {
-    axios.post('http://localhost:3001/login', {
-      username:username,
-      password:password
-    })
-    .then(response => {
+  
+  async function login() {
+    await axios.post('http://localhost:3001/login', {
+      username: username, 
+      password: password
+      
+    }).then(response => {
       console.log(response)
       console.log(response.data.token)
       if (response.data.token) {
-        storeToken(response.data.token)
-        dispatch(setToken(tempToken))
+       
+      localStorage.setItem('token', response.data.token)
+       console.log(localStorage.getItem('token'))
       }
     })
     .catch(error => setError(error))
   }
   
+  console.log(localStorage.getItem('token'))
   
   return (
     <div className='flex flex-col p-1 items-center'>
@@ -43,7 +46,7 @@ export default function Login() {
               <label for='password'>Password: </label>
               <input type='password' name='password' defaultValue={password} onChange={(e)=>setPassword(e.target.value)}></input>
             </div>
-            <div className="justify-center flex bg-blue-100 rounded-md shadow-md"><button onClick={() => login()}>Login</button></div>
+            <div className="justify-center flex bg-blue-100 rounded-md shadow-md"><button onClick={async () => await login()}>Login</button></div>
           </div>
         </form>
       <span></span>
