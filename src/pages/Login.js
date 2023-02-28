0c81d994a42e800813dab5efd1bf4d2793335e12
anthony <margherioa@gmail.com>
2023-02-27 23:55:35 -0600

    added login success/failure text to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,25 +7,24 @@ import {useSelector, useDispatch} from 'react-redux'
 export default function Login() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
+  const [response, setResponse] = useState()
   const [tempToken, storeToken] = useState('')
+  const [error, setError] = useState('')
   //useSelector selects a state value from initial state
   const token = useSelector(selectUserToken)
   //dispatch defines the action that feeds into the reducer (ie, what are we doin here with this piece state?)
   const dispatch = useDispatch()
-  console.log(token)
   async function login() {
     axios.post('http://localhost:3001/login', {
       username:username,
       password:password
     })
     .then(response => {
-      console.log(response)
+      setResponse(response.data.message)
       console.log(response.data.token)
       if (response.data.token) {
        
       localStorage.setItem('token', response.data.token)
-       console.log(localStorage.getItem('token'))
       }
     })
     .catch(error => setError(error))
@@ -48,7 +47,7 @@ export default function Login() {
             <div className="justify-center flex bg-blue-100 rounded-md shadow-md"><button type='button' onClick={() => login()}>Login</button></div>
           </div>
         </form>
-      <span></span>
+      <span>{response}</span>
     </div>
   )
 }
